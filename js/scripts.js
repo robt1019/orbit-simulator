164fa189fda3cdde5912d41e2cd0fdfbd4a2883a
Rob Taylor <robert.i.taylor88@gmail.com>
2020-04-15 21:32:55 +0100

    configure planet as center of system

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,48 @@ const planetSystem = function (canvas) {
   const ctx = canvas.getContext("2d");
 
   const _planets = [];
+  let center;
   let biggestPlanetRadius = 0;
   let conversionFactor;
 
   const addPlanet = function (planet) {
-    _planets.push(planet);
+    if (planet.isCenterOfSystem) {
+      center = planet;
+    } else {
+      _planets.push(planet);
+    }
     if (planet.radius > biggestPlanetRadius) {
       biggestPlanetRadius = planet.radius;
     }
-    conversionFactor = canvas.height / (biggestPlanetRadius * 3);
   };
 
   const draw = function () {
+    conversionFactor = canvas.height / (biggestPlanetRadius * 5);
     clearCanvas();
+    drawPlanet(center);
+
     _planets.forEach(function (planet) {
-      const [x, y] = planet.position;
-      ctx.beginPath();
-      ctx.fillStyle = planet.color;
-      ctx.arc(x, y, planet.radius * conversionFactor, 0, 2 * Math.PI);
-      ctx.fill();
+      drawPlanet(planet);
     });
+  };
+
+  const drawPlanet = function (planet) {
+    const [x, y] = planet.position;
+    ctx.beginPath();
+    ctx.fillStyle = planet.color;
+    ctx.arc(
+      (x / 100) * canvas.width,
+      (y / 100) * canvas.height,
+      planet.radius * conversionFactor,
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
   };
 
   const clearCanvas = function () {
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
   return {
@@ -35,27 +52,28 @@ const planetSystem = function (canvas) {
   };
 };
 
-const solarSystemSimulator = function (canvas) {
-  const solarSystem = planetSystem(canvas);
+const sunMoonSystemSimulator = function (canvas) {
+  const system = planetSystem(canvas);
 
-  solarSystem.addPlanet({
+  system.addPlanet({
     name: "Earth",
     radius: 6371000,
     mass: 5.972 * Math.pow(10, 24),
-    position: [30, 50],
+    position: [50, 50],
     color: "green",
+    isCenterOfSystem: true,
   });
 
-  solarSystem.addPlanet({
-    name: "Sun",
-    radius: 696340000,
-    mass: 1.989 * Math.pow(10, 30),
-    position: [200, 200],
-    color: "red",
+  system.addPlanet({
+    name: "Moon",
+    radius: 1736000,
+    mass: 1.348 * Math.pow(10, 22),
+    position: [90, 90],
+    color: "grey",
   });
 
   return {
-    drawSolarSystem: solarSystem.draw,
+    drawSolarSystem: system.draw,
   };
 };
 
@@ -64,9 +82,19 @@ window.addEventListener("load", function () {
     "#startSimulationButton"
   );
 
-  const system = solarSystemSimulator(document.querySelector("#orbitCanvas"));
+  const canvas = document.querySelector("#orbitCanvas");
 
-  startSimulationButton.addEventListener("click", () => {
-    system.drawSolarSystem();
+  const system = sunMoonSystemSimulator(canvas);
+  canvas.height = window.innerHeight * 0.9;
+  canvas.width = window.innerWidth * 0.99;
+
+  window.addEventListener("resize", function () {
+    setTimeout(function () {
+      canvas.height = window.innerHeight * 0.9;
+      canvas.width = window.innerWidth * 0.99;
+      system.drawSolarSystem();
+    });
   });
+
+  system.drawSolarSystem();
 });
